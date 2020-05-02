66757f5bbc7847769f40a52ebca0e30d795e43a3
himap2569 <60684923+himap2569@users.noreply.github.com>
2020-05-02 10:11:01 +0530

    Add files via upload

---
+++ LEDon.ino
@@ -0,0 +1,9 @@
+void setup() {
+pinMode(D0,OUTPUT);
+//pinMode(D0,INTPUT);
+}
+
+void loop() {
+digitalWrite(D0,LOW);
+//digitalRead(D0,1);
+}
---
+++ Printing_Statement.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+//9600-bit rate
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.println("Hello VRSEC");
+delay(1000);
+}
